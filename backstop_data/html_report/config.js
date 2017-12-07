4bb7b26c452fd5a4679d69f712b1b8396f2bce39
Lei Qi <qileilove@gmail.com>
2017-12-07 11:00:01 +0800

    [QL] init and use backstopjs to compare the ui test

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,245 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../..",
+        "test": "../..",
+        "selector": "",
+        "fileName": "",
+        "label": "",
+        "scenario": {
+          "label": "index page",
+          "cookiePath": "backstop_data/engine_scripts/cookies.json",
+          "url": "https://www.squarefoot.com.hk/property/sai-kung/tin-liu/16300865/",
+          "referenceUrl": "",
+          "readyEvent": "",
+          "readySelector": "",
+          "delay": 35000,
+          "hideSelectors": [],
+          "removeSelectors": [],
+          "hoverSelector": "",
+          "clickSelector": "",
+          "postInteractionWait": "",
+          "selectors": [
+            "ol",
+            ".property-summary",
+            "#bottom-contact-agent"
+          ],
+          "selectorExpansion": true,
+          "misMatchThreshold": 0.1,
+          "requireSameDimensions": true,
+          "sIndex": 0
+        },
+        "viewport": {
+          "label": "tablet",
+          "width": 1920,
+          "height": 1080,
+          "vIndex": 0
+        },
+        "msg": "Chromy error",
+        "error": "Chromy error: GotoTimeoutError. See scenario – index page (tablet)"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../..",
+        "test": "../..",
+        "selector": "",
+        "fileName": "",
+        "label": "",
+        "scenario": {
+          "label": "click .ant-tabs .displayToggle a",
+          "cookiePath": "backstop_data/engine_scripts/cookies.json",
+          "url": "https://www.squarefoot.com.hk/property/sai-kung/tin-liu/16300865/",
+          "referenceUrl": "",
+          "readyEvent": "",
+          "readySelector": "#adSlot-3",
+          "delay": 35000,
+          "hideSelectors": [
+            "#adSlot-1",
+            "#adSlot-2",
+            "#adSlot-3"
+          ],
+          "removeSelectors": [],
+          "hoverSelector": "",
+          "clickSelector": ".ant-tabs .ant-tabs-tabpane a",
+          "postInteractionWait": "",
+          "selectors": [
+            ".ant-tabs .container"
+          ],
+          "selectorExpansion": true,
+          "misMatchThreshold": 0.1,
+          "requireSameDimensions": true,
+          "sIndex": 1
+        },
+        "viewport": {
+          "label": "tablet",
+          "width": 1920,
+          "height": 1080,
+          "vIndex": 0
+        },
+        "msg": "Chromy error",
+        "error": "Chromy error: GotoTimeoutError. See scenario – click .ant-tabs .displayToggle a (tablet)"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../..",
+        "test": "../..",
+        "selector": "",
+        "fileName": "",
+        "label": "",
+        "scenario": {
+          "label": "click .right-contact-agent button",
+          "cookiePath": "backstop_data/engine_scripts/cookies.json",
+          "url": "https://www.squarefoot.com.hk/property/sai-kung/tin-liu/16300865/",
+          "referenceUrl": "",
+          "readyEvent": "",
+          "readySelector": ".right-contact-agent",
+          "delay": 35000,
+          "hideSelectors": [],
+          "removeSelectors": [],
+          "hoverSelector": "",
+          "clickSelector": ".right-contact-agent button",
+          "postInteractionWait": "",
+          "selectors": [
+            ".right-contact-agent .contact-form-top"
+          ],
+          "selectorExpansion": true,
+          "misMatchThreshold": 0.1,
+          "requireSameDimensions": true,
+          "sIndex": 2
+        },
+        "viewport": {
+          "label": "tablet",
+          "width": 1920,
+          "height": 1080,
+          "vIndex": 0
+        },
+        "msg": "Chromy error",
+        "error": "Chromy error: GotoTimeoutError. See scenario – click .right-contact-agent button (tablet)"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../..",
+        "test": "../..",
+        "selector": "",
+        "fileName": "",
+        "label": "",
+        "scenario": {
+          "label": "get image title",
+          "cookiePath": "backstop_data/engine_scripts/cookies.json",
+          "url": "https://www.squarefoot.com.hk/property/sai-kung/tin-liu/16300865/",
+          "referenceUrl": "",
+          "readyEvent": "",
+          "readySelector": "#adSlot-3",
+          "delay": 35000,
+          "hideSelectors": [],
+          "removeSelectors": [],
+          "hoverSelector": ".image-bar .icon",
+          "clickSelector": ".image-bar .icon",
+          "postInteractionWait": ".header",
+          "selectors": [
+            ".header"
+          ],
+          "selectorExpansion": true,
+          "misMatchThreshold": 0.1,
+          "requireSameDimensions": true,
+          "sIndex": 3
+        },
+        "viewport": {
+          "label": "tablet",
+          "width": 1920,
+          "height": 1080,
+          "vIndex": 0
+        },
+        "msg": "Chromy error",
+        "error": "Chromy error: GotoTimeoutError. See scenario – get image title (tablet)"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../..",
+        "test": "../..",
+        "selector": "",
+        "fileName": "",
+        "label": "",
+        "scenario": {
+          "label": "get image title",
+          "cookiePath": "backstop_data/engine_scripts/cookies.json",
+          "url": "https://www.squarefoot.com.hk/sale/?placeId=41-0&placeId=21-0&place=wan-chai&place=chai-wan",
+          "referenceUrl": "",
+          "readyEvent": "",
+          "readySelector": ".search",
+          "delay": 35000,
+          "hideSelectors": [],
+          "removeSelectors": [],
+          "hoverSelector": "",
+          "clickSelector": "",
+          "postInteractionWait": ".search",
+          "selectors": [
+            ".search"
+          ],
+          "selectorExpansion": true,
+          "misMatchThreshold": 0.1,
+          "requireSameDimensions": true,
+          "sIndex": 4
+        },
+        "viewport": {
+          "label": "tablet",
+          "width": 1920,
+          "height": 1080,
+          "vIndex": 0
+        },
+        "msg": "Chromy error",
+        "error": "Chromy error: GotoTimeoutError. See scenario – get image title (tablet)"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../..",
+        "test": "../..",
+        "selector": "",
+        "fileName": "",
+        "label": "",
+        "scenario": {
+          "label": "get image title",
+          "cookiePath": "backstop_data/engine_scripts/cookies.json",
+          "url": "https://www.squarefoot.com.hk/sale/wan-chai",
+          "referenceUrl": "",
+          "readyEvent": "",
+          "readySelector": ".search",
+          "delay": 35000,
+          "hideSelectors": [],
+          "removeSelectors": [],
+          "hoverSelector": "",
+          "clickSelector": "",
+          "postInteractionWait": ".search",
+          "selectors": [
+            ".search"
+          ],
+          "selectorExpansion": true,
+          "misMatchThreshold": 0.1,
+          "requireSameDimensions": true,
+          "sIndex": 5
+        },
+        "viewport": {
+          "label": "tablet",
+          "width": 1920,
+          "height": 1080,
+          "vIndex": 0
+        },
+        "msg": "Chromy error",
+        "error": "Chromy error: GotoTimeoutError. See scenario – get image title (tablet)"
+      },
+      "status": "fail"
+    }
+  ]
+});
